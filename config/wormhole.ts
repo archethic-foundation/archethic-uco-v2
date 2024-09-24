430532488972d0d670ddd399ae22496d07e8cb15
MB2M <aphal.dev@gmail.com>
2024-09-24 14:15:46 +0200

    Update wormhole.ts
    update wormhole config for mainnet

--- config/wormhole.ts
+++ config/wormhole.ts
@@ -1,48 +1,81 @@
 import { WormholeConnectConfig } from "@wormhole-foundation/wormhole-connect";
 
 export const wormholeConfig: WormholeConnectConfig = {
-  env: "testnet",
-  networks: ["sepolia", "bsc"],
+  env: "mainnet",
+  networks: ["polygon", "bsc", "ethereum"],
   tokensConfig: {
-    UCOSepolia: {
-      key: "UCOSepolia",
+    UCOEthereum: {
+      key: "UCOEthereum",
       symbol: "UCO",
-      nativeChain: "sepolia",
+      nativeChain: "ethereum",
       tokenId: {
-        chain: "sepolia",
-        address: "0xEBd9C8279e8208E8C5C3De23eE831cfF27D8CDba",
+        chain: "ethereum",
+        address: "0x1A688D3d294ee7BcC1f59011DE93d608Dc21c377",
       },
-      coinGeckoId: "basket",
-      icon: "https://assets.coingecko.com/coins/images/34661/standard/BSKT_Logo.png?1705636891",
-      color: "#2894EE",
+      coinGeckoId: "archethic",
+      icon: "https://assets.coingecko.com/coins/images/12330/standard/Archethic_Logo.png?1719358185",
+      color: "#ee2828",
       decimals: {
-        default: 18,
+        default: 8,
       },
       foreignAssets: {
         bsc: {
-          address: "0x857F7DcC5327648c54fF43f8b348272661B1AedC",
-          decimals: 18,
+          address: "0xf1e5bbd997501a8439619266A09a54b2b499eAA3",
+          decimals: 8,
+        },
+        polygon: {
+          address: "0xaa53B93608C88EE55fAD8db4C504Fa20E52642aD",
+          decimals: 8,
         },
       },
     },
-    UCOBSCTestnet: {
-      key: "UCOBSCTestnet",
+    UCOBsc: {
+      key: "UCOBsc",
       symbol: "UCO",
       nativeChain: "bsc",
       tokenId: {
         chain: "bsc",
         address: "0x857F7DcC5327648c54fF43f8b348272661B1AedC",
       },
-      coinGeckoId: "basket",
-      icon: "https://assets.coingecko.com/coins/images/34661/standard/BSKT_Logo.png?1705636891",
+      coinGeckoId: "archethic",
+      icon: "https://assets.coingecko.com/coins/images/12330/standard/Archethic_Logo.png?1719358185",
       color: "#2894EE",
       decimals: {
-        default: 18,
+        default: 8,
       },
       foreignAssets: {
-        sepolia: {
-          address: "0xEBd9C8279e8208E8C5C3De23eE831cfF27D8CDba",
-          decimals: 18,
+        ethereum: {
+          address: "0x1A688D3d294ee7BcC1f59011DE93d608Dc21c377",
+          decimals: 8,
+        },
+        polygon: {
+          address: "0xaa53B93608C88EE55fAD8db4C504Fa20E52642aD",
+          decimals: 8,
+        },
+      },
+    },
+    UCOPolygon: {
+      key: "UCOPolygon",
+      symbol: "UCO",
+      nativeChain: "polygon",
+      tokenId: {
+        chain: "polygon",
+        address: "0xaa53B93608C88EE55fAD8db4C504Fa20E52642aD",
+      },
+      coinGeckoId: "archethic",
+      icon: "https://assets.coingecko.com/coins/images/12330/standard/Archethic_Logo.png?1719358185",
+      color: "#2894EE",
+      decimals: {
+        default: 8,
+      },
+      foreignAssets: {
+        bsc: {
+          address: "0xf1e5bbd997501a8439619266A09a54b2b499eAA3",
+          decimals: 8,
+        },
+        ethereum: {
+          address: "0x1A688D3d294ee7BcC1f59011DE93d608Dc21c377",
+          decimals: 8,
         },
       },
     },
@@ -51,31 +84,39 @@ export const wormholeConfig: WormholeConnectConfig = {
     UCO: {
       nttManagers: [
         {
-          address: "0x38276Ad0B68999BB894ac07161656028Dd4fBddD",
-          chainName: "sepolia",
-          tokenKey: "UCOSepolia",
+          address: "0xB16Bf54398A913799D537C0c472394AeA4Fb8EfC",
+          chainName: "ethereum",
+          tokenKey: "UCOEthereum",
           transceivers: [
             {
-              address: "0xEbFb99345440e7E38bB2DcFD30DB154E5B136385",
+              address: "0xC8d43BA20c6CE5F3dB8bF66F547940D422225b7c",
               type: "wormhole",
             },
           ],
         },
         {
-          address: "0xAa68D75Ee94F3563965ABb64DA6835a1E0f15756",
+          address: "0x01c07123229a583168536E2061A461602F0C1f79",
           chainName: "bsc",
-          tokenKey: "UCOBSCTestnet",
+          tokenKey: "UCOBsc",
           transceivers: [
             {
-              address: "0x864723FBf0BEA188530E5afbF7cDE6351f5Eb20e",
+              address: "0x6B13D454b86e32d2C0eda1738c56327e28b1784F",
+              type: "wormhole",
+            },
+          ],
+        },
+        {
+          address: "0x7BD106409a543B8Ca77Ef618754aA9c913B89011",
+          chainName: "polygon",
+          tokenKey: "UCOPolygon",
+          transceivers: [
+            {
+              address: "0x5183A428785f7d6aF139C7E68962FE203Cb141Dd",
               type: "wormhole",
             },
           ],
         },
       ],
     },
-  },
-  rpcs: {
-    sepolia: "https://ethereum-sepolia.publicnode.com",
   },
 };
